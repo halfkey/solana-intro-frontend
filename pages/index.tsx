2117ddcabffa747b9df2cf0631e1621ab07b836d
7figurelarp <113866580+7figurelarp@users.noreply.github.com>
2022-12-01 09:56:46 -0800

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,20 @@ import * as Web3 from '@solana/web3.js'
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
+  const [isExecutable, setIsExecutable] = useState('')
 
   const addressSubmittedHandler = (address: string) => {
     try {
       setAddress(address)
       const key = new Web3.PublicKey(address)
       const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+      
       connection.getBalance(key).then(balance => {
         setBalance(balance / Web3.LAMPORTS_PER_SOL)
+      })
+
+      connection.getAccountInfo(key).then(info => {
+        setIsExecutable(info?.executable ?? false);
       })
     } catch (error) {
       setAddress('')
@@ -23,15 +29,18 @@ const Home: NextPage = () => {
     }
   }
 
+ 
+
   return (
     <div className={styles.App}>
       <header className={styles.AppHeader}>
         <p>
-          Start Your Solana Journey
+        😈traps devnet solana balance checker😈
         </p>
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is it executable? ${isExecutable}`}</p>
       </header>
     </div>
   )
